fd0919c1bcfae66a6ba0130e1492a63bd5079ad4
macsj200 <maxjohansen@berkeley.edu>
2017-07-15 14:46:50 -0700

    static bio data

---
+++ gatstrap/components/OfficerBio/OfficerBioData.js
@@ -0,0 +1,25 @@
+/*
+  Sample
+
+  Name: Clara Lee
+  Officer Position: competition director
+  Years Climbing: 1.2 yuh
+  Favorite Discipline: bouldering all the way I don"t got no money for all the other gear
+  Climbing Pet Peeve: someone eating something greasy and then getting on the problem
+  Sending Food: chipotle tortilla
+  Favorite Climbing Quote: Alex honnold after he free soloed el cap: "I"m gonna go hang board now" ?????
+ */
+
+const officerBiosData = [
+  {
+    name: "Clara Lee",
+    officerPosition: "competition director",
+    yearsClimbing: "1.2 yuh",
+    favoriteDiscipline: "bouldering all the way I don\"t got no money for all the other gear",
+    climbingPetPeeve: "someone eating something greasy and then getting on the problem",
+    sendingFood: "chipotle tortilla",
+    favoriteClimbingQuote: "Alex honnold after he free soloed el cap: \"I'm gonna go hang board now\" ?????",
+  },
+]
+
+export default officerBiosData
