edc5a75957e08c477d3f21483fc24f2df45cd478
macsj200 <maxjohansen@berkeley.edu>
2017-07-15 15:36:33 -0700

    bios to front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,32 @@
 import React from "react"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
+import OfficerBio from "../components/OfficerBio/OfficerBio.jsx";
+import officerBiosData from "../components/OfficerBio/OfficerBioData.js";
 
 export default class Index extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Cal Climbing Website</h1>
-        <p>
-          This is the official website for Cal Climbing, the climbing club at <a href="http://www.berkeley.edu/">UC Berkeley</a>.
-        </p>
-        <Link to="/officer-bios/">Current officers</Link>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Cal Climbing Website</h1>
+            <p>
+              This is the official website for Cal Climbing, the climbing club at <a href="http://www.berkeley.edu/">UC Berkeley</a>.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h1>Current Cal Climbing Officers</h1>
+            <h2>Fall 2017 Semester</h2>
+            <ul className="list-group">
+              {
+                officerBiosData.map(officerBioData => <OfficerBio key={officerBioData.name} {...officerBioData} />)
+              }
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
