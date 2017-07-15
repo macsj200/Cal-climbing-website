841c0fc39efecd0eddf521908f7cad2c830fd124
macsj200 <maxjohansen@berkeley.edu>
2017-07-15 15:14:01 -0700

    add bootstrap w00t

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
 
+import "bootstrap/dist/css/bootstrap.min.css"
 import "../css/typography.css"
 
 export default class Template extends React.Component {
