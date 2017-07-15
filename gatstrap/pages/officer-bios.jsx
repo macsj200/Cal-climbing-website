f1883e90b9a99856b86d19d2117da913867f6631
macsj200 <maxjohansen@berkeley.edu>
2017-07-15 14:58:14 -0700

    add officer bio page

---
+++ gatstrap/pages/officer-bios.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default class OfficerBios extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Current Cal Climbing Officers</h1>
+        <h2>Fall 2017 Semester</h2>
+        {/*
+          officerBiosData.map(officerBioData => <OfficerBio key={officerBioData.name} {...officerBioData} />)
+        */}
+      </div>
+    )
+  }
+}
