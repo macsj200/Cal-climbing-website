9916edfa62add4fc01911ac1fc5ef343fb535890
macsj200 <maxjohansen@berkeley.edu>
2017-07-15 15:05:01 -0700

    modularize

--- src/pages/officer-bios.js
+++ src/pages/officer-bios.js
@@ -2,57 +2,8 @@ import React from "react"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
 import PropTypes from "prop-types"
-
-/*
-  Sample
-
-  Name: Clara Lee
-  Officer Position: competition director
-  Years Climbing: 1.2 yuh
-  Favorite Discipline: bouldering all the way I don't got no money for all the other gear
-  Climbing Pet Peeve: someone eating something greasy and then getting on the problem
-  Sending Food: chipotle tortilla
-  Favorite Climbing Quote: Alex honnold after he free soloed el cap: "I'm gonna go hang board now" ?????
- */
-
-const officerBiosData = [
-  {
-    name: 'Clara Lee',
-    officerPosition: 'competition director',
-    yearsClimbing: '1.2 yuh',
-    favoriteDiscipline: 'bouldering all the way I don\'t got no money for all the other gear',
-    climbingPetPeeve: 'someone eating something greasy and then getting on the problem',
-    sendingFood: 'chipotle tortilla',
-    favoriteClimbingQuote: 'Alex honnold after he free soloed el cap: "I\'m gonna go hang board now" ?????',
-  },
-];
-
-const OfficerBio = ({
-  name,
-  officerPosition,
-  yearsClimbing,
-  favoriteDiscipline,
-  climbingPetPeeve,
-  sendingFood,
-  favoriteClimbingQuote,
-}) => {
-  return (
-    <div>
-      <h4>{name}</h4>
-      <h5>{officerPosition}</h5>
-    </div>
-  );
-}
-
-OfficerBio.propTypes = {
-  name: PropTypes.string.isRequired,
-  officerPosition: PropTypes.string.isRequired,
-  yearsClimbing: PropTypes.string.isRequired,
-  favoriteDiscipline: PropTypes.string.isRequired,
-  climbingPetPeeve: PropTypes.string.isRequired,
-  sendingFood: PropTypes.string.isRequired,
-  favoriteClimbingQuote: PropTypes.string.isRequired,
-}
+import OfficerBio from "../components/OfficerBio/OfficerBio.jsx";
+import officerBiosData from "../components/OfficerBio/OfficerBioData.js";
 
 export default class OfficerBios extends React.Component {
   render() {
